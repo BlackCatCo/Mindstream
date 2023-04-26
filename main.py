638e135e1accb95db21d5426dcfaadf9f452e80c
jimmydin7 <129774378+jimmydin7@users.noreply.github.com>
2023-04-26 13:42:15 +0300

    Added all the code

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from operator import itemgetter
+import datetime
+import json
+import uuid
+
+app = Flask(__name__)
+
+db_file = 'db.json'
+try:
+    with open(db_file) as f:
+        posts = json.load(f)
+except FileNotFoundError:
+    posts = []
+
+@app.route("/")
+def index():
+    return redirect("/feed")
+
+
+@app.route("/feed", methods=["GET", "POST"])
+def feed():
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        post_id = str(uuid.uuid4())
+        post = {"id": post_id, "title": title, "description": description, "timestamp": timestamp}
+        posts.insert(0, post)
+        with open(db_file, 'w') as f:
+            json.dump(posts, f, indent=4)
+    posts_sorted = sorted(posts, key=itemgetter("timestamp"), reverse=True)
+    return render_template("base.html", posts=posts_sorted)
+
+
+@app.route("/feed/share/<id>")
+def share(id):
+    post = next((p for p in posts if p["id"] == id), None)
+    if not post:
+        return "Post not found"
+    return render_template("shared.html", post=post)
+
+if __name__ == '__main__':
+    app.run(debug=True)
